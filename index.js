707770997995ed555e2d04efdaf929cd4b787e37
191225 <191225yuichirou@gmail.com>
2022-09-24 03:32:01 +0900

    :

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { clientId, guildId, token } = require('./config.json');
 
-const GuildCommands = [
-	new SlashCommandBuilder()
-		.setName('error')
-		.setDescription('Displays errors that have occurred.'),
-	new SlashCommandBuilder()
-		.setName('test')
-		.setDescription('test')
-]
-	.map(command => command.toJSON());
-
 const GlobalCommands = [
 	new SlashCommandBuilder()
 		.setName('help')
@@ -21,25 +11,26 @@ const GlobalCommands = [
 		.setDescription('APIの応答速度を返します。'),
 	new SlashCommandBuilder()
 		.setName('panel')
-		.setDescription('パネルを作成します。'),
-	new SlashCommandBuilder()
-		.setName('createInvitePanel')
-		.setDescription('宣伝用のカスタムパネルを作成します。'),
+		.setDescription('パネルを作成します。')
+		.addSubcommand(subcommand => 
+			subcommand
+				.setName('Unknown Panel')
+				.setDescription('Unknownサーバーのパネルを作成します。')
+			)
+		.addSubcommand(subcommand => 
+			subcommand
+				.setName('Costom Panel')
+				.setDescription('カスタムパネルを作成します。')
+			)
+		
 ]
 	.map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
 
 rest.put(Routes.applicationCommands(clientId), { body: [] })
 	.then(() => console.log('Successfully deleted all application commands.'))
-	.catch(console.error);
-
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: GuildCommands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
 
 rest.put(Routes.applicationCommands(clientId), { body: GlobalCommands })
